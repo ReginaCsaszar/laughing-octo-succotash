c4216c30603a5796b200a784f5fc3abb48522c1b
ReginaCsaszar <regina.csaszar@gmail.com>
2017-06-15 17:07:25 +0200

    resolve CE button conflict with operators

--- script.js
+++ script.js
@@ -1,53 +1,87 @@
 
-//document.getElementById("neg")
-//document.getElementById("dot")
+//document.getElementById("undo")
 //document.getElementById("result")
 
 var content = "0";
 var numbers = document.getElementsByClassName("number");
 var operators = document.getElementsByClassName("operator");
+var float = false;
 var i;
 
-var display = function(info) {
-    if (info.length > 25) {
-        info = info.slice(-25);
+var display = function(chars) {
+// handle content printing to display
+    if (chars.length > 25) {
+        info = chars.slice(-25);
+    }
+    else {
+        info = chars;
     }
     document.getElementById("display").innerHTML = info;
 };
 
-var removeLastOperator = function(chars) {
-    var operatorArray = ["%", "÷", "x", "-", "+"];
+var isOperator = function(chars) {
+// check if last char is operator
+    var operatorArray = ["%", "÷", "x", "-", "+", "."];
     var lastChar = chars.slice(-1);
     if (operatorArray.indexOf(lastChar) != -1) {
-        chars = chars.slice(0, -1);
+        return true;
     }
-    return chars;
+    return false;
 };
 
 for (i = 0; i < numbers.length; i++) {
+// handle number buttons
     numbers[i].onclick = function() {
         if (content === "0") {
             content = "";
         }
         content = content + this.innerHTML;
         if (content === "00") {
-            content = "0";
+        content = "0";
         }
         display(content);
     }
 }
 
 for (i = 0; i < operators.length; i++) {
+// handle operator buttons
     operators[i].onclick = function() {
-        if (content != "0") {
-            content = removeLastOperator(content);
-            content = content + this.innerHTML;
+        if (isOperator(content)) {
+            content = content.slice(0, -1);
         }
+        content = content + this.innerHTML;
+        float = false;
         display(content);
     }
 }
 
+document.getElementById("dot").onclick = function(){
+// handle dot button
+    if (!float) {
+        if (isOperator(content)) {
+            content = content + "0";
+        }
+        content = content + ".";
+        float = true;
+    }
+    display(content);
+}
+
+document.getElementById("undo").onclick = function(){
+// handle CE button
+    if (content.slice(-1) === ".") {
+        float = false;
+    }
+    content = content.slice(0, -1);
+    if (content === "") {
+        content = "0";
+    }
+    display(content);
+}
+
 document.getElementById("clear").onclick = function(){
+// handle C button
     content = "0";
+    float = false;
     display(content);
 }
